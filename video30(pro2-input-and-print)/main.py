8100d7a8f9606d5fc499cb2844bf8f37d07a45c7
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-08-04 11:39:32 +0330

    pro2-input-and-print

---
+++ video30(pro2-input-and-print)/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the group name generator")
+color = input("What is Your favorite color? ")
+animal = input("What is your favorite animal? ")
+print("Your group name could be %s %ss" % (color, animal))
