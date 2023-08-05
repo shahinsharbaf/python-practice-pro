7c8ac35fce088146f04493a8b98b4dba9dd27dec
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-08-05 17:14:02 +0330

    wowwwwwww

---
+++ video69(function-documatation-beautiful-awsome)/main.py
@@ -0,0 +1,8 @@
+# document documatation is way to descripe function behaveir for users
+# after we callback func this documantation is shown to
+def concat(str1, str2):
+    """" this is documantation this function for concate two strings """
+    return (str1+str2)
+
+
+concat()
