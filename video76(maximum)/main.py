6119964fe3d31b05232370c491f189b954a82d7d
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-08-06 17:31:46 +0330

    better

--- video76(maximum)/main.py
+++ video76(maximum)/main.py
@@ -1,13 +1,33 @@
-def max(n1, n2, n3):
+# def max(n1, n2, n3):
+#     if n1 > n2:
+#         if n1 > n3:
+#             return n1
+#         else:
+#             return n3
+#     if n2 > n3:
+#         return n2
+#     else:
+#         return n3
+
+
+# try:
+#     n1 = int(input("Enter first number? "))
+#     n2 = int(input("Enter secondnumber? "))
+#     n3 = int(input("Enter third number? "))
+# except:
+#     print("not valid")
+# else:
+#     print(max(n1, n2, n3))
+
+def max_two_number(n1, n2):
     if n1 > n2:
-        if n1 > n3:
-            return n1
-        else:
-            return n3
-    if n2 > n3:
-        return n2
+        return n1
     else:
-        return n3
+        return n2
+
+
+def max_there_number(n1, n2, n3):
+    return max_two_number(max_two_number(n1, n2), n3)
 
 
 try:
@@ -17,4 +37,4 @@ try:
 except:
     print("not valid")
 else:
-    print(max(n1, n2, n3))
+    print(max_there_number(n1, n2, n3))
