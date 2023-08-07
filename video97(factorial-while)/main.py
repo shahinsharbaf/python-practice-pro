72c6ea2f34233b2cf44d037881754d170b37b1a6
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-08-07 11:44:45 +0330

    factoria-while

---
+++ video97(factorial-while)/main.py
@@ -0,0 +1,17 @@
+def factorial(facrorial_num):
+    if facrorial_num < 0:
+        return ("factoria does not exist")
+    elif facrorial_num == 0:
+        return (1)
+    result = 1
+    for num in range(1, facrorial_num+1):
+        result = result * num
+    return (result)
+
+
+try:
+    factorial_num = int(input("Enter factorial num: "))
+except:
+    print("not valid")
+else:
+    print(factorial(factorial_num))
