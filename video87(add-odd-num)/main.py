9e2766864756ef6fcb44591301cfea272996c2ce
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-08-07 11:04:16 +0330

    while

---
+++ video87(add-odd-num)/main.py
@@ -0,0 +1,4 @@
+sum_odd = 0
+for item in range(1, 101, 2):
+    sum_odd += item
+print(sum_odd)
---
+++ video92(while-loop)/main.py
@@ -0,0 +1,4 @@
+username = []
+username.append(input("Enter username: "))
+while username[len(username)-1] != "test":
+    username.append(input("Enter username: "))
